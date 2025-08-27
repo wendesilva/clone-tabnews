02425a9186bb1ab2572c6eb7ad2bc12f4391ecb7
Wendel Silva <wendesilva.ws@hotmail.com>
2025-08-27 00:29:49 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para melhorar a habilidade em programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá! Tem café aí hoje? </h1>
+  return <h1>Olá! Tem café aí hoje? </h1>;
 }
 
 export default Home;
